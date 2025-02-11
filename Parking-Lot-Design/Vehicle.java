2aefe5483a7945c062e13a5f8b4c0047faae9687
Sourav Walke <46669210+souravwalke@users.noreply.github.com>
2025-02-11 12:27:58 -0800

    Create Vehicle.java

---
+++ Parking-Lot-Design/Vehicle.java
@@ -0,0 +1,14 @@
+abstract public class Vehicle {
+
+    protected String licensePlate;
+    protected VehicleType type;
+
+    public Vehicle(String licensePlate, VehicleType type) {
+        this.licensePlate = licensePlate;
+        this.type = type;
+    }
+
+    public VehicleType getType() {
+      return type;
+    }
+}
